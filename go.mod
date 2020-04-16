71261e17a846485c602c3e026f2fd4976ed87ddd
rreinold <rreinold@gmail.com>
2020-04-16 14:39:06 -0500

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module str
+
+go 1.12
+
+require (
+	github.com/dgraph-io/dgo/v2 v2.2.0
+	github.com/gin-gonic/gin v1.6.2 // indirect
+	google.golang.org/grpc v1.28.1
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"encoding/json"    
+	"github.com/dgraph-io/dgo/v2"
+	"github.com/dgraph-io/dgo/v2/protos/api"
+    
+	"google.golang.org/grpc"
+	"github.com/gin-gonic/gin"
+)
+
+func initThis(){
+	fmt.Printf("Ayo")
+
+}
+
+func main() {
+	var (
+		dgraph = flag.String("d", "127.0.0.1:9080", "Dgraph Alpha address")
+	)
+	flag.Parse()
+
+	initThis()
+
+	conn, err := grpc.Dial(*dgraph, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	gdb := dgo.NewDgraphClient(api.NewDgraphClient(conn))
+
+	router := gin.Default()
+	router.GET("/graph", func(c *gin.Context) {
+		fmt.Printf("Called /graph")
+		graph := getGraph(gdb)
+		fmt.Printf("%+v",graph)
+		c.JSON(200, graph)
+	})
+	router.Run(":3000")
+}
+
+func getGraph(dgb *dgo.Dgraph) map[string]interface{}{
+    
+	resp, err := dgb.NewTxn().Query(context.Background(), `{
+	  topic(func: has(storyline)) {
+	    uid
+	    topic_title
+	    storyline {
+	      name
+	      event {
+	        name
+	        place {
+	          name
+	        }
+	      }
+	    }
+	  }
+	}`)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	var response map[string]interface{}
+	err = json.Unmarshal(resp.Json, &response)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%+v", response)
+	return response
+}
